8a63442a958047231d21073f5acb043454ad6179
tanmen <yt.prog@gmail.com>
2019-12-24 14:57:59 +0900

    test Input group

--- src/inputs/Input.tsx
+++ src/inputs/Input.tsx
@@ -33,20 +33,20 @@ const Css = {
   size: {
     default: css`
       height: calc(1rem + .75rem + 2px);
-      padding: .375rem .75rem;
+      padding: .375rem .5rem;
       font-size: 1rem;
       border-radius: .25rem;
     `,
     small: css`
       height: calc(.875rem + .50rem + 2px);
-      padding: .25rem .5rem;
+      padding: .25rem .25rem;
       font-size: .875rem;
       line-height: 1.5;
       border-radius: .2rem;
     `,
     large: css`
       height: calc(1.25rem + 1rem + 2px);
-      padding: .5rem 1rem;
+      padding: .5rem .75rem;
       font-size: 1.25rem;
       line-height: 1.5;
       border-radius: .3rem;
--- src/inputs/InputGroup.tsx
+++ src/inputs/InputGroup.tsx
@@ -1,13 +1,46 @@
-import React, {FC} from "react";
-import styled from "styled-components";
+import React, {createContext, FC} from "react";
+import styled, {css} from "styled-components";
+import {ColorType} from "../types/ColorType";
+import {SizeType} from "../types/SizeType";
 
-const InputGroup: FC = ({children}) => <Group className="input-group">
-  {children}
-</Group>;
+const {Provider, Consumer} = createContext({size: SizeType.Normal, color: ColorType.Primary});
+
+type Props = { size?: SizeType, color?: ColorType };
+const InputGroup: FC<Props> = ({
+                                 size = SizeType.Normal,
+                                 color = ColorType.Primary,
+                                 children
+                               }) =>
+  <Provider value={{size, color}}>
+    <Group size={size} className="input-group">
+      {children}
+    </Group>
+  </Provider>;
 
 export default InputGroup;
 
-export const Prepend = styled.div`
+const Css = {
+  size: {
+    default: css`
+      height: calc(1rem + .75rem + 2px);
+    `,
+    small: css`
+      height: calc(.875rem + .50rem + 2px);
+    `,
+    large: css`
+      height: calc(1.25rem + 1rem + 2px);
+    `,
+  }
+};
+
+export const Prepend: FC = ({children}) =>
+  <Consumer>
+    {value => <SPrepend {...value}>
+      {children}
+    </SPrepend>}
+  </Consumer>;
+
+const SPrepend = styled.div<Required<Props>>`
 display: flex;
 margin-right: -1px;
 background-color: #e9ecef;
@@ -23,6 +56,7 @@ border-radius: .25rem 0 0 .25rem;
   border-top-right-radius: 0;
   border-bottom-right-radius: 0;
 }
+${({size}) => Css.size[size]}
 `;
 
 export const PrependText = styled.span`
@@ -38,7 +72,7 @@ text-align: center;
 white-space: nowrap;
 `;
 
-export const Group = styled.div`
+export const Group = styled.div<{ size: SizeType }>`
 position: relative;
 display: flex;
 flex-wrap: wrap;
