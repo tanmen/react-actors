d4f4e071d56992530a725db50f416133f0fae28d
tanmen <yt.prog@gmail.com>
2022-06-25 18:54:23 +0900

    chore(storybook): add theme property

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -2,22 +2,26 @@ import {ThemeProvider} from "../src/providers";
 import React from "react";
 
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
-  argTypes: {
-    mode: {
-      control: {
-        type: 'radio',
-        options: ['light', 'dark']
-      },
-      defaultValue: 'light'
+  actions: {argTypesRegex: "^on[A-Z].*"}
+}
+
+export const argTypes = {
+  mode: {
+    control: {
+      type: 'radio',
+      options: ['light', 'dark']
     }
   }
+}
+
+export const args = {
+  mode: 'light'
 }
 
 export const decorators = [
   (Story, {args: {mode}}) => (
     <ThemeProvider mode={mode}>
-      <Story />
+      <Story/>
     </ThemeProvider>
   )
 ];
