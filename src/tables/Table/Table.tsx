e8c145194505e7bd89b580867d219314a3c0735b
tanmen <yt.prog@gmail.com>
2023-02-24 03:05:54 +0900

    add sort

--- src/tables/Table/Table.tsx
+++ src/tables/Table/Table.tsx
@@ -3,10 +3,12 @@ import React, {FC, MouseEvent, ReactNode} from "react";
 import {useMode} from "../../hooks";
 import {ModeType} from "../../types";
 
+type Align = 'right' | 'left' | 'center';
 type Column<T extends DataModel, K extends keyof T = keyof T> = {
   Header: ReactNode,
-  accessor: K
-  Wrapper?: FC<{ data: T, accessor: K, value: T[K], children: ReactNode }>
+  accessor: K,
+  align: Align
+  Wrapper?: FC<{ data: T, accessor: K, align: Align, value: T[K], children: ReactNode }>
   onClick?: (props: { data: T, accessor: K, value: T[K] }, event: MouseEvent) => Promise<any> | any
 }
 type DataModel = { [key: string]: ReactNode };
@@ -29,10 +31,10 @@ export const Table =
       <tbody>
       {data.map((row, index) =>
         <Tr key={index} className={onClick ? 'clickable' : ''} onClick={onClick ? (e) => onClick(row, e) : undefined}>
-          {columns.map(({Wrapper, accessor, onClick: onCellClick}) =>
-            <Td key={[index, accessor].join('-')} mode={mode} className={onCellClick ? 'clickable' : ''}
+          {columns.map(({Wrapper, accessor, align = 'left', onClick: onCellClick}) =>
+            <Td key={[index, accessor].join('-')} mode={mode} align={align} className={onCellClick ? 'clickable' : ''}
                 onClick={onCellClick ? (e) => onCellClick({data: row, accessor, value: row[accessor]}, e) : undefined}>
-              {Wrapper ? <Wrapper data={row} accessor={accessor} value={row[accessor]}>
+              {Wrapper ? <Wrapper data={row} accessor={accessor} align={align} value={row[accessor]}>
                 {row[accessor]}
               </Wrapper> : <>{row[accessor]}</>}
             </Td>)}
@@ -52,8 +54,9 @@ const Tr = styled.tr`
   }
 `;
 
-const Td = styled.td<{ mode: ModeType }>`
+const Td = styled.td<{ mode: ModeType, align: Align }>`
   padding: .5rem;
+  text-align: ${({align}) => align};
 
   tbody > tr:not(:last-of-type) > & {
     border-bottom: 1px solid ${({mode}) => mode === 'light' ? '#dee2e6' : '#464b4f'};
