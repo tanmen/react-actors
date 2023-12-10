0d1a78a18d46fb137115dd4aac24c6211dd366b6
tanmen <yt.prog@gmail.com>
2023-12-10 23:11:51 +0900

    fix(dropdown): add props

--- src/dropdowns/SearchDropdown/SearchDropdown.tsx
+++ src/dropdowns/SearchDropdown/SearchDropdown.tsx
@@ -1,4 +1,4 @@
-import {FC, forwardRef, ReactNode, useState} from "react";
+import {DetailedHTMLProps, FC, forwardRef, InputHTMLAttributes, ReactNode, useState} from "react";
 import {Input} from "../../inputs/Input";
 import {Dropdown} from "../Dropdown/Dropdown";
 
@@ -10,15 +10,15 @@ export type SearchDropdownItem = {
 export type SearchDropdownProps = {
   items: SearchDropdownItem[]
   onTextChange?: (text: string) => Promise<unknown> | unknown;
-}
+} & Omit<DetailedHTMLProps<InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>, 'type' | 'value' | 'ref'>
 
 export const SearchDropdown: FC<SearchDropdownProps> = forwardRef<HTMLInputElement, SearchDropdownProps>(
-  ({items, onTextChange}, ref) => {
+  ({items, onTextChange, ...props}, ref) => {
     const [value, setValue] = useState<string>();
     const [open, setOpen] = useState(false);
     return <Dropdown open={open}
                      items={items.map(({key, body}) => <div key={key} onClick={() => setValue(key)}>{body}</div>)}>
-      <input ref={ref} type="hidden" value={value}/>
+      <input ref={ref} type="hidden" value={value} {...props}/>
       <Input onFocus={() => setOpen(true)} onBlur={() => setOpen(false)}
              onChange={e => onTextChange?.(e.target.value)}/>
     </Dropdown>;
