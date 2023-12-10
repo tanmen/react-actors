757c19f339b9d6c2a22cd156731b57dde5b835c0
tanmen <yt.prog@gmail.com>
2023-12-10 23:47:19 +0900

    fix(dropdown): set value

--- src/dropdowns/SearchDropdown/SearchDropdown.tsx
+++ src/dropdowns/SearchDropdown/SearchDropdown.tsx
@@ -11,15 +11,15 @@ export type SearchDropdownProps = {
   items: SearchDropdownItem[]
   onTextChange?: (text: string) => Promise<unknown> | unknown;
   className?: string;
-} & Omit<DetailedHTMLProps<InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>, 'type' | 'value' | 'ref' | 'className'>
+} & Omit<DetailedHTMLProps<InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>, 'type'  | 'ref' | 'className'>
 
 export const SearchDropdown: FC<SearchDropdownProps> = forwardRef<HTMLInputElement, SearchDropdownProps>(
-  ({items, onTextChange,className, ...props}, ref) => {
-    const [value, setValue] = useState<string>();
+  ({items, onTextChange,className,value, ...props}, ref) => {
+    const [_value, setValue] = useState<string | readonly string[] | number | undefined>(value);
     const [open, setOpen] = useState(false);
     return <Dropdown className={className} open={open}
                      items={items.map(({key, body}) => <div key={key} onClick={() => setValue(key)}>{body}</div>)}>
-      <input ref={ref} type="hidden" value={value} {...props}/>
+      <input ref={ref} type="hidden" value={_value} {...props}/>
       <Input className="react-actors-search-dropdown-input" onFocus={() => setOpen(true)} onBlur={() => setOpen(false)}
              onChange={e => onTextChange?.(e.target.value)}/>
     </Dropdown>;
